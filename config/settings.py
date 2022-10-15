528e3f8b487efc34967286dbb1e53e82e131f2ad
Bethu05 <bethwellangat05@gmail.com>
2022-10-15 12:50:24 +0300

    Change dependencies

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from decouple import config
+from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
